da4e7ba63733dab33e8b4749d44c385166f770df
Nayem Hossain <nayemsb12@gmail.com>
2026-01-05 16:05:59 +0600

    app test

---
+++ flask/flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+'''
+It creates an instance of the Flask class, which will be your WSGI (Web Server Gateway Interface) application.
+'''
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
+
