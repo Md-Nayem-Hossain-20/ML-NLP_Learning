5e1e3576ea8c1fc9b6d62165e3355c0dd7cf9997
Nayem Hossain <nayemsb12@gmail.com>
2026-01-05 16:08:16 +0600

    function added

--- flask/flask/app.py
+++ flask/flask/app.py
@@ -4,6 +4,10 @@ from flask import Flask
 It creates an instance of the Flask class, which will be your WSGI (Web Server Gateway Interface) application.
 '''
 
+def main():
+    return "Hello World"
+
+
 app = Flask(__name__)
 
 if __name__ == "__main__":
