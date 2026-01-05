e533ccac76b890f7fe05830468351cbd975bc0e3
Nayem Hossain <nayemsb12@gmail.com>
2026-01-05 16:44:37 +0600

    basic skeleton of flask

--- flask/flask/app.py
+++ flask/flask/app.py
@@ -4,12 +4,17 @@ from flask import Flask
 It creates an instance of the Flask class, which will be your WSGI (Web Server Gateway Interface) application.
 '''
 
-def main():
-    return "Hello World"
-
 
 app = Flask(__name__)
 
+@app.route("/")
+def welcome():
+    return "Welcome to this page.And it will restart server because of debug=True"
+
+@app.route("/index")
+def index():
+    return "This is index page"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
